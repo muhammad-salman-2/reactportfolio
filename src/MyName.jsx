f62175cfaa46f007bc57efaf3577227c6268d5a6
Muhammad Salman <connectwithsalman25@gmail.com>
2025-02-15 22:19:40 -0800

    first commit

--- src/MyName.jsx
+++ src/MyName.jsx
@@ -5,7 +5,7 @@ const MyName = () => {
     <>
     <div className=' text-white tracking-wide container items-center flex pl-[77px] w-full  justify-start '>
     <a href="">
-          <p className="motion-preset-slide-left motion-delay-500 motion-preset-fade  motion-duration-[4s] rounded-full  flex items-center font-bold text-2xl h-28 w-60 duration-300 hover:ease-out hover:scale-125 " >Muhammad Salman</p>
+          <p className="motion-preset-slide-left motion-delay-500 motion-preset-fade  motion-duration-[4s] rounded-full  flex items-center font-bold text-2xl h-28 w-60 duration-300 hover:ease-out hover:scale-125  " >Muhammad Salman</p>
     </a>
 
     {/* <div className=' tracking-wide container flex pl-[77px] w-full   justify-start '>
